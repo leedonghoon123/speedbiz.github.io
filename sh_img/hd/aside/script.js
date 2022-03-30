daabb8e3a67d6351e4b50892801f761cb6bd3eb7
leedonghoon123 <speed_1029@naver.com>
2022-03-30 11:09:20 +0900

    hoon
    1

---
+++ sh_img/hd/aside/script.js
@@ -0,0 +1,14 @@
+﻿$(function(){
+ //$("li a").hover li의 a노드에 마우스 오버, 아웃 했을때 함수 호출
+ $(".l_menu_OFF a").hover(
+   function(){
+    //현재 a태그에 작동되는 애니메이션을 중지하고, [stop() = stop(false,false)]
+    //background-color와 텍스트 색상을 설정한다.
+    $(this).stop().animate({"background-color":"#636363","color":"#FFF"},300);
+   },
+   //마우스 아웃이 되면, a태그에 있는 애니메이션을 중지하고 
+   //background-color와 텍스트 색상을 설정한다.
+   function(){
+    $(this).stop().animate({"background-color":"#ededed","color":"#333333"},300);
+   });
+});
---
+++ sh_img/hd/top_menu/script.js
@@ -0,0 +1,19 @@
+﻿// top_nav
+$(document).ready(function() {	
+	$('#top_nav li').hover(function() {
+		$('ul', this).slideDown(200);
+		$(this).children('a:first').addClass("hov");
+	}, function() {
+		$('ul', this).slideUp(100);
+		$(this).children('a:first').removeClass("hov");		
+	});
+});
+
+
+// ie6
+$(function(){
+	$('.product_list li:first-child').css("border-top","0");  
+	$('.tbl_ir.black tr td').css("border-right","0");  
+});
+
+
